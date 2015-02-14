c8f91c934649e092e4157218ebce20798b15db05
jonathan enrique moran duran <jonathan-duran@hotmail.com>
2015-02-14 10:41:00 -0600

    Inicio de proyecto
    Agreados:
    - Biblioteca de clases InvDataAccess para manipulacion de datos.
    - Complemento de excel 2013 como plataforma del sistema.

---
+++ Control de inventario/InvRibbon.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Control_de_inventario
+{
+    partial class InvRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public InvRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = Factory.CreateRibbonTab();
+            this.group1 = Factory.CreateRibbonGroup();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pestaña1
+            // 
+            this.tab1.Name = "tab1";
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.ControlId.OfficeId = "TabAddIns";
+            this.tab1.Groups.Add(this.group1);
+            // 
+            // grupo1
+            // 
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // InvRibbon
+            // 
+            this.Name = "InvRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.InvRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal InvRibbon InvRibbon
+        {
+            get { return this.GetRibbon<InvRibbon>(); }
+        }
+    }
+}
